6994b116a54572cba542a554ddf86138de7248a6
kedarsutar613 <kedarsutar613@gmail.com>
2024-03-14 18:18:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Profiler } from 'react'
+import Home from './pages/home/Home'
+// import Register from './pages/register/Register'
+// import Login from './pages/login/Login'
+// import Profile from './pages/profile/Profile'
+
+// import {BrowserRouter as Router, Switch, Route, Redirect,} from "react-router-dom";
+const App = () => {
+  return (
+    <>
+   {/* <Login/> */}
+   {/* <Register/> */}
+   <Home/>
+   {/* <Profile/> */}
+
+
+
+  </>
+  )
+}
+export default App
+
+
